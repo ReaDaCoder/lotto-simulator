6fc6acf9a27453f226fe821243901ff4f47da865
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-07-09 08:43:29 +0200

    created variables and functions

--- script.js
+++ script.js
@@ -1,46 +1,28 @@
 // Initialize variables
-DEFINE lottoNumbers as an empty list
-DEFINE userNumbers as an empty list
-DEFINE matchingNumbers as an empty list
-DEFINE numberOfNumbers as 6
-DEFINE maxNumber as 49
+let lottoNum = [];
+let userNum = [];
+let matchNum = [];
+let numberOfNum =[];
+let maxNum = [];
 
 // Function to generate random lotto numbers
-FUNCTION generateLottoNumbers()
-  WHILE lottoNumbers.size < numberOfNumbers DO
-    GENERATE randomNumber between 1 and maxNumber
-    IF randomNumber is not in lottoNumbers THEN
-      ADD randomNumber to lottoNumbers
-    END IF
-  END WHILE
-END FUNCTION
+function createNum(){}
+  
 
-// Function to get user numbers
-FUNCTION getUserNumbers()
-  PRINT "Enter your lotto numbers (between 1 and maxNumber):"
-  WHILE userNumbers.size < numberOfNumbers DO
-    INPUT userNumber
-    IF userNumber is between 1 and maxNumber AND userNumber is not in userNumbers THEN
-      ADD userNumber to userNumbers
-    ELSE
-      PRINT "Invalid number, try again."
-    END IF
-  END WHILE
-END FUNCTION
+// The function below will return user's input number
+function getInputNum(){
+    return "Enter your lotto numbers (between 1 and maxNumber):"
+}
 
-// Function to compare numbers
-FUNCTION compareNumbers()
-  FOR EACH number IN userNumbers DO
-    IF number is in lottoNumbers THEN
-      ADD number to matchingNumbers
-    END IF
-  END FOR
-END FUNCTION
+  
 
-// Main program execution
-generateLottoNumbers()
-getUserNumbers()
-compareNumbers()
+
+function checkNum(){}
+  
+
+createNum()
+getInputNum()
+checkNum()
 
 
 
